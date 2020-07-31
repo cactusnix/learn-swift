03dfb7bbb587c42feedf2e93264647d60fd12d1a
cactusnix <cocalemontea@gmail.com>
2020-07-31 21:32:13 +0800

    7.31 2 day

--- Basic.swift
+++ Basic.swift
@@ -1,6 +1,6 @@
 //
 //  Basic.swift
-//  
+//
 //
 //  Created by 朱嘉新 on 2020/7/13.
 //
@@ -11,9 +11,9 @@ let constValue = "string"
 
 // optional value
 var optionalString: String? = "hello"
-print(optionalString == nil )
+print(optionalString == nil)
 
-var optionalName: String? = nil
+var optionalName: String?
 var greeting = "Hello"
 // 判断 name
 if let name = optionalName {
@@ -21,6 +21,7 @@ if let name = optionalName {
 } else {
     greeting = "hello, else"
 }
+
 print(greeting)
 
 let nickname: String? = "test optional nickname"
@@ -72,3 +73,94 @@ repeat {
 
 print(m, n)
 
+// 语法糖
+var total = 0
+for i in 0 ..< 4 {
+    total += i
+}
+
+print("test total", total)
+
+var total1 = 0
+for i in 0 ... 6 {
+    total1 += i
+}
+
+print("test total", total1)
+
+// function
+func helloWord(msg: String) -> String {
+    return "Hello \(msg)"
+}
+
+print(helloWord(msg: "word"))
+
+func returnMultiple(array: [Int]) -> (min: Int, max: Int, sum: Int) {
+    var min = array[0]
+    var max = 0
+    var sum = 0
+    for v in array {
+        if v < min {
+            min = v
+        }
+        if v > max {
+            max = v
+        }
+        sum += v
+    }
+    return (min, max, sum)
+}
+
+print(returnMultiple(array: [23, 4343, 534, 2312, 55, 45634]))
+
+func add(var1: Int, var2: Int) -> Int {
+    return var1 + var2
+}
+
+func returnCanBeAdd(var1: Int, var2: Int) -> Int {
+    if var1 > 10 {
+        return add(var1: var1, var2: var2)
+    } else {
+        return 0
+    }
+}
+
+print(returnCanBeAdd(var1: 9, var2: 10))
+print(returnCanBeAdd(var1: 19, var2: 10))
+
+// return function
+func returnFunc() -> ((Int) -> Int) {
+    func addOne(num: Int) -> Int {
+        return num + 1
+    }
+    return addOne
+}
+
+var increment = returnFunc()
+
+print(increment(100))
+
+// function argument
+func hasLessThanNum(list: [Int], condition: (Int, Int) -> Bool) -> Bool {
+    for v in list {
+        if condition(v, 100) {
+            return true
+        }
+    }
+    return false
+}
+
+func lessThanNum(var1: Int, num: Int) -> Bool {
+    return var1 < num
+}
+
+print("test less than 100", hasLessThanNum(list: [3213, 53254, 333, 645], condition: lessThanNum))
+
+// 诡异的闭包 num 作为入参，返回值在 in 后面
+print([1,434,66].map({(num: Int) -> Int in
+    if num % 2 == 1 {
+        return 0
+    } else {
+        return 1
+    }
+}))
---
+++ ListToTree.swift
@@ -0,0 +1,17 @@
+//
+//  ListToTree.swift
+//
+//
+//  Created by 朱嘉新 on 2020/7/24.
+//
+
+let list = [
+    {
+        id: 1,
+        pid: 0,
+        label: "a"
+    },
+    {
+        
+    }
+]
