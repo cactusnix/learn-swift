45afec8e6ca1f1dee15e615751536303006a046b
cactusnix <cocalemontea@gmail.com>
2020-08-09 21:47:01 +0800

    8.9 4 day

---
+++ LanguageGuide.swift
@@ -0,0 +1,71 @@
+//
+//  LanguageGuide.swift
+//
+//
+//  Created by 朱嘉新 on 2020/8/7.
+//
+
+/*
+ 常量与变量
+ 1. 调用前声明
+ 2. 常量不能修改，变量可以修改
+ 3. 单行多声明
+ 4. 命名规范
+    1. 常量和变量名称不能包含空格字符，数学符号，箭头，专用的Unicode标量值或线条和框形图字符。
+    它们也不能以数字开头，尽管数字可能包含在名称中的其他位置。
+ */
+let constantVar = 10
+var changeVar = 0
+var var1 = 0, var2 = "3"
+let var3 = 333, var4 = "333"
+
+/*
+ 类型注解
+ */
+var typeAnnotaiton: String
+typeAnnotaiton = "typeAnnotation"
+var type1, type2, type3: Bool
+
+/*
+ 注释
+ // 单行注释
+ /*
+ 多行注释，可以嵌套
+ */
+ */
+
+/*
+ 分号
+ 不需要写
+ */
+
+/*
+ 整型
+ 使用Int即可，无需使用Integer
+ */
+
+
+/*
+ 类型推断
+ type infer
+ */
+
+/*
+ 类型转换必须都是显示声明的
+ 整型之间的转换 Int8 -> Int16
+ 整型->浮点型
+ */
+
+/*
+ 类型别名
+ typealias AudioSample = UInt16
+ 一次定义，处处使用
+ */
+
+/*
+ 布尔型
+ */
+
+/*
+ 元组
+ */
